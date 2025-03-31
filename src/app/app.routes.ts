08cdf528595617f3b5de8b70e335b1eb62c0a7b8
Ricard_VE <ricard_ve@disroot.org>
2025-03-31 22:26:39 +0200

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddHeroFormComponent } from './components/add-hero-form/add-hero-form.component';
 import { EditHeroFormComponent } from './components/edit-hero-form/edit-hero-form.component';
 
+
 export const routes: Routes = [
     { path: '', redirectTo: '/heroes', pathMatch: 'full'},
     { path: 'heroes', component: DashboardComponent},
