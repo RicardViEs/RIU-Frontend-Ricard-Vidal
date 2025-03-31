34cfbf0f59938df81ba14f5d30f295a3e7a32e77
Ricard_VE <ricard_ve@disroot.org>
2025-03-31 16:14:32 +0200

    complete hero service

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -1,16 +1,176 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed,inject } from '@angular/core/testing';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { HeroService } from './hero.service';
+import { Observable } from 'rxjs';
+import { Hero } from '../models/hero';
 
 describe('HeroService', () => {
-  let service: HeroService;
+  let heroService: HeroService;
+  let url = '/assets/heroes.json';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroService);
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [provideHttpClient(), provideHttpClientTesting(), HeroService]
+    });
+    heroService = TestBed.inject(HeroService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(heroService).toBeTruthy();
   });
+
+  it('should get heroes', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+      const mockHeroes = [
+        {id: '1', name: 'Vision', description: 'A hero who can see a lot', superPower: 'superVision'},
+        {id: '2', name: 'Strengthy', description: 'A hero who is super strong', superPower: 'superStrength'},
+        {id: '3', name: 'The Smarty', description: 'A hero who is super smart', superPower: 'superSmart'},
+        {id: '4', name: 'Multitron', description: 'A hero with multiple super powers', superPower: 'superStrength'},
+      ];
+
+      heroService.getHeroes().subscribe((event: Observable<Hero[]>) => {
+        event.subscribe((heroes: Hero[]) => {
+          expect(heroes).toEqual(mockHeroes);
+          expect(heroes.length).toEqual(4);
+        }) 
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush(mockHeroes);
+
+      httpMock.verify();
+    }
+  ));
+
+  it('should get hero by id', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+      const mockHero = {id: '1', name: 'Vision', description: 'A hero who can see a lot', superPower: 'superVision'};
+
+      heroService.getHeroById('1').subscribe((hero: Hero | undefined) => {
+        expect(hero).toEqual(mockHero);
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush(mockHero);
+
+      httpMock.verify();
+    }
+  ));
+
+  it('should get undefined', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+      const mockHero = {id: '1', name: 'Vision', description: 'A hero who can see a lot', superPower: 'superVision'};
+
+      heroService.getHeroById('999').subscribe((hero: Hero | undefined) => {
+        expect(hero).toBeUndefined();
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush(mockHero);
+
+      httpMock.verify();
+    }
+  ));
+
+
+  it('should update hero', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+      const mockHero = {id: '1', name: 'Vision', description: 'A hero who can see a lot', superPower: 'supaVision'};
+
+      heroService.updateHero(mockHero).subscribe((heroes: Hero[]) => {
+        heroes.forEach((hero: Hero) => {
+          if (hero.id === '1') {
+            expect(hero.name).toEqual(mockHero.name);
+          }
+        })
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush(mockHero);
+
+      httpMock.verify();
+    }
+  ));
+
+  it('should add a new hero', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+
+      heroService.addHeroe('Invisible man', 'A hero who can turn invisible', 'invisibility').subscribe((heroes: Hero[]) => {
+        expect(heroes.length).toBe(5);
+        heroes.forEach((hero: Hero) => {
+          if (hero.id === '5') {
+            expect(hero.name).toEqual('Invisible man');
+          }
+        })
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush({name: 'Invisible man', description: 'A hero who can turn invisible', superPower: 'invisibility'});
+
+      httpMock.verify();
+    }
+  ));
+
+  it('should delete a hero', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+
+      heroService.deleteHero('1').subscribe((heroes: Hero[]) => {
+        expect(heroes.length).toBe(3);
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush('1');
+
+      httpMock.verify();
+    }
+  ));
+
+  it('should return heros containing strin', inject(
+    [HttpTestingController, HeroService],
+    (httpMock: HttpTestingController, heroService: HeroService) => {
+
+      heroService.getHeroesContainingStringInName('vis').subscribe((heroes: Hero[]) => {
+        expect(heroes.length).toBe(1);
+        expect(heroes[0].name).toBe('Vision');
+      });
+
+      const mockReq = httpMock.expectOne(url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush('1');
+
+      httpMock.verify();
+    }
+  ));
+
+
+
 });
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,105 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { Hero } from '../models/hero';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  httpService = inject(HttpClient);
+  private readonly url = '/assets/heroes.json';
+
+  private readonly heroes$ = new BehaviorSubject<Hero[]>([
+    {id: '1', name: 'Vision', description: 'A hero who can see a lot', superPower: 'superVision'},
+    {id: '2', name: 'Strengthy', description: 'A hero who is super strong', superPower: 'superStrength'},
+    {id: '3', name: 'The Smarty', description: 'A hero who is super smart', superPower: 'superSmart'},
+    {id: '4', name: 'Multitron', description: 'A hero with multiple super powers', superPower: 'superStrength'},
+  ]);
+
+  private readonly resetHeroes$ = new BehaviorSubject<Hero[]>([
+    {id: '1', name: 'Vision', description: 'A hero who can see a lot', superPower: 'superVision'},
+    {id: '2', name: 'Strengthy', description: 'A hero who is super strong', superPower: 'superStrength'},
+    {id: '3', name: 'The Smarty', description: 'A hero who is super smart', superPower: 'superSmart'},
+    {id: '4', name: 'Multitron', description: 'A hero with multiple super powers', superPower: 'superStrength'},
+  ]);
+
+  getHeroes(){
+    return this.httpService.get<Hero[]>(this.url, {}).pipe(
+      map(() => this.heroes$.asObservable())
+    )
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpService.get<Hero>(this.url).pipe(
+      map(()=> {
+        const heroe = this.heroes$.getValue().find(( element ) => element.id === id)
+        return heroe;
+      })
+    );
+  }
+
+  updateHero(hero: Hero): Observable<Hero[]>{
+    return this.httpService.put<Hero[]>(this.url, hero).pipe(
+      map(()=> {
+        const currentState = this.heroes$.getValue();
+        const newState = currentState.map(( item ) => {
+          return (item.id === hero.id) ? hero : item
+        })
+        this.heroes$.next(newState);
+        return newState;
+      })
+    )
+  }
+
+  addHeroe(name: string, description: string, superPower: string): Observable<Hero[]> {
+    const currentState = this.heroes$.getValue();
+    const id = currentState.length + 1;
+
+    const newHero: Hero = {
+      id: String(id),
+      name: name,
+      description: description,
+      superPower: superPower
+    }
+
+    return this.httpService.post<Hero[]>(this.url, newHero).pipe(
+      map(() => {
+        this.heroes$.next([...currentState, ...[newHero]]);
+        this.resetHeroes$.next([...currentState, ...[newHero]]);
+        return this.heroes$.getValue();
+      })
+    );    
+    
+  }
+
+  deleteHero(id: string | undefined) {
+    const currentState = this.heroes$.getValue();
+    const newState = currentState.filter(( item ) => item.id !== id);
+    this.heroes$.next( newState );
+    this.resetHeroes$.next( newState );
+    return this.httpService.delete<string>(this.url).pipe(
+      map(() => {
+        return this.heroes$.getValue();
+      })
+    )
+  }
+
+  getHeroesContainingStringInName(str: string | null | undefined) {
+    if (str && str.length >= 1) {
+      this.heroes$.next(this.resetHeroes$.getValue());
+      const newState = this.heroes$.getValue().filter(( element ) => (element.name.toLowerCase()).includes(str));
+      this.heroes$.next( newState );
+    } else {
+      this.heroes$.next(this.resetHeroes$.getValue());
+    }
+    return this.httpService.get<string>(this.url).pipe(
+      map(() => {
+        return this.heroes$.getValue();
+      })
+    )
+      
+  }
+
 }
